b7a7b1e1b25087d5431dc22896dd5748226586d6
awwal148 <adesaanyaawwal446@gmail.com>
2024-01-12 09:23:04 -0800

    added a loading page

---
+++ app/loading.js
@@ -0,0 +1,17 @@
+
+function Loading() {
+
+  return (
+    <>
+        <div className="loading flex justify-center items-center h-screen">
+          <p className="text-2xl font-bold text-blue-500">
+            Loading<span className="animate-pulse">.</span>
+            <span className="animate-pulse">.</span>
+            <span className="animate-ping">.</span>
+          </p>
+        </div>
+    </>
+  );
+}
+
+export default Loading;
